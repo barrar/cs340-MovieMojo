4b6ff8dc613782f78f3e6b325f4f3c8be0ffe947
barrar <barrar@users.noreply.github.com>
2020-03-03 07:10:14 +0000

    reCAPTCHA, menu, features list

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -1,25 +1,31 @@
 var express = require('express');
 var mysqlConnection = require('../mysqlConnection');
 var router = express.Router();
+var recaptcha = require('../recaptcha');
 
-router.get('/createAccount', function(req, res) {
-    res.render('createAccount');
+router.get('/createAccount', recaptcha.middleware.render, function(req, res) {
+    res.render('createAccount', { "recaptcha": res.recaptcha });
 });
 
-router.post('/createAccount', function(req, res) {
-    var connection = mysqlConnection();
-    connection.query(
-        `INSERT INTO users (name, email, password) VALUES (:name, :email, :password)`,
-        req.body,
-        function(err, rows, fields) {
-            if (err) {
-                console.log(err);
-                res.render('createAccount', { "status": 0 });
-            }
-            res.render('createAccount', { "status": 1 });
-        });
+router.post('/createAccount', recaptcha.middleware.render, recaptcha.middleware.verify, function(req, res) {
+    if (req.recaptcha.error) {
+        res.render('createAccount', { "recaptcha": res.recaptcha, "status": 0 });
 
-    connection.end();
+    } else {
+        var connection = mysqlConnection();
+        connection.query(
+            `INSERT INTO users (name, email, password) VALUES (:name, :email, :password)`,
+            req.body,
+            function(err, rows, fields) {
+                if (err) {
+                    console.log(err);
+                    res.render('createAccount', { "recaptcha": res.recaptcha, "status": 0 });
+                }
+                res.redirect('/signIn');
+            });
+
+        connection.end();
+    }
 });
 
 module.exports = router;
--- routes/signIn.js
+++ routes/signIn.js
@@ -1,36 +1,38 @@
 var express = require('express');
-var moment = require('moment');
 var mysqlConnection = require('../mysqlConnection');
 var router = express.Router();
+var recaptcha = require('../recaptcha');
 
-router.get('/signIn', function(req, res) {
-    var connection = mysqlConnection();
-    res.render('signIn');
-    connection.end();
+router.get('/signIn', recaptcha.middleware.render, function(req, res) {
+    res.render('signIn', { "recaptcha": res.recaptcha });
 });
 
-router.post('/signIn', function(req, res) {
-    var connection = mysqlConnection();
-    connection.query(
-        `SELECT * FROM moviemojo.users
+router.post('/signIn', recaptcha.middleware.render, recaptcha.middleware.verify, function(req, res) {
+    if (req.recaptcha.error) {
+        res.render('signIn', { "recaptcha": res.recaptcha, "status": 0 });
+    } else {
+        var connection = mysqlConnection();
+        connection.query(
+            `SELECT * FROM moviemojo.users
         WHERE email=:email and password=:password;`,
-        req.body,
-        function(err, rows, fields) {
-            if (err) {
-                console.log(err);
-                res.render('signIn', { "status": 0 });
-            }
-            if (rows.length > 0) {
-                req.session.userID = rows[0].userID;
-                req.session.name = rows[0].name;
-                req.session.email = rows[0].email;
-                req.session.password = rows[0].password;
-                res.redirect('/');
-            } else {
-                res.render('signIn', { "status": 0 });
-            }
-        });
-    connection.end();
+            req.body,
+            function(err, rows, fields) {
+                if (err) {
+                    console.log(err);
+                    res.render('signIn', { "recaptcha": res.recaptcha, "status": 0 });
+                }
+                if (rows.length > 0) {
+                    req.session.userID = rows[0].userID;
+                    req.session.name = rows[0].name;
+                    req.session.email = rows[0].email;
+                    req.session.password = rows[0].password;
+                    res.redirect('/');
+                } else {
+                    res.render('signIn', { "recaptcha": res.recaptcha, "status": 0 });
+                }
+            });
+        connection.end();
+    }
 });
 
 router.get('/signIn/signOut', function(req, res) {
