5c0d3c51a9c5000bdca9305c3d39df0460ffc491
barrar <barrar@users.noreply.github.com>
2020-03-10 08:51:20 +0000

    Faster movies/actors pages

--- routes/actors.js
+++ routes/actors.js
@@ -1,7 +1,18 @@
 var router = require('express').Router();
+var moment = require('moment');
 
 router.get('/actors', function(req, res) {
-    res.render('actors');
+    mysqlPool.query(`SELECT * FROM actors`, function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+            res.render('error');
+        } else {
+            for (var i = 0; i < rows.length; i++) {
+                rows[i].birthday = moment(rows[i].birthday).format('MMM Do, YYYY');
+            }
+            res.render('actors', { "actorList": rows });
+        }
+    });
 });
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,24 @@
 var router = require('express').Router();
+var moment = require('moment');
 
 router.get('/movies', function(req, res) {
-    res.render('movies');
+    mysqlPool.query(`SELECT * FROM movies`, function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+            res.render('error');
+        } else {
+            let movieList = [];
+            for (var i = 0; i < rows.length; i++) {
+                var movie = {
+                    'text': rows[i].name + " - released " + moment(rows[i].releaseDate).format('MMM Do, YYYY'),
+                    'movieID': rows[i].movieID
+                }
+                movieList.push(movie);
+            }
+            res.render('movies', { "movieList": movieList });
+        }
+    });
 });
+
 
 module.exports = router;
